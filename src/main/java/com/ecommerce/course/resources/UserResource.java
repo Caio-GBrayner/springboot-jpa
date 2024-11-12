7522cf8b71f81dfabe5bb5246b5f71b50759e4af
Caio Gomes Brayner <caiogomesbrayner@gmail.com>
2024-11-12 15:41:02 -0300

    User entity and resource

---
+++ src/main/java/com/ecommerce/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.ecommerce.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "caio", "caio@gmail.com", "(81)98289100", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+}
