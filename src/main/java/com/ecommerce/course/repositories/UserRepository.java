9c272c10fc081dc59448c5981b83bc01ff3c3703
Caio Gomes Brayner <caiogomesbrayner@gmail.com>
2024-11-12 16:35:01 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/ecommerce/course/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerce.course.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecommerce.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	
+
+}
