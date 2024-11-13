f5e0f0e8cadd147b6d62a04ca35ece9fa7ee3178
Caio Gomes Brayner <caiogomesbrayner@gmail.com>
2024-11-13 10:37:52 -0300

    Service layer, component registration

--- src/main/java/com/ecommerce/course/repositories/UserRepository.java
+++ src/main/java/com/ecommerce/course/repositories/UserRepository.java
@@ -1,11 +1,12 @@
 package com.ecommerce.course.repositories;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ecommerce.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	
